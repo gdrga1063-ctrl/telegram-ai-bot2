887b0eb1d1a130fd28da64fe60b675161a66a4b5
gdrga1063-ctrl <gdrga1063@gmail.com>
2026-04-29 22:42:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,6 +55,7 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     print("Бот запущен...")
+    app.run_polling(drop_pending_updates=True)
     app.run_polling()
 
 
