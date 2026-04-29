0ad9945ce28c6b9a8431687579899ab2c01cd513
gdrga1063-ctrl <gdrga1063@gmail.com>
2026-04-29 23:29:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def ai_generate(user_input, state):
                 "Content-Type": "application/json"
             },
             json={
-    "model": "qwen/qwen-2.5-14b-instruct",
+    "model": "openai/gpt-3.5-turbo",
     "messages": [
         {"role": "system", "content": "Ты странный, немного живой ИИ. Отвечай коротко и иногда необычно."},
         {"role": "user", "content": user_input}
