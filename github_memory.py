f6a789c0613c3dcecac551dfc855310e04c58772
gdrga1063-ctrl <gdrga1063@gmail.com>
2026-05-16 13:54:19 +0300

    Create github_memory.py

---
+++ github_memory.py
@@ -0,0 +1,49 @@
+from github import Github
+import os
+import json
+
+TOKEN = os.getenv("GITHUB_TOKEN")
+REPO_NAME = "Arslan1063/MyTelegramAI_UwU"
+
+g = Github(TOKEN)
+repo = g.get_repo(REPO_NAME)
+
+FILE_PATH = "diary.json"
+
+
+def update_github_diary(entry):
+    try:
+        try:
+            file = repo.get_contents(FILE_PATH)
+
+            content = json.loads(file.decoded_content.decode())
+
+        except:
+            content = []
+
+        content.append(entry)
+
+        new_content = json.dumps(
+            content,
+            ensure_ascii=False,
+            indent=2
+        )
+
+        try:
+            repo.update_file(
+                FILE_PATH,
+                "Обновление дневника ИИ",
+                new_content,
+                file.sha
+            )
+        except:
+            repo.create_file(
+                FILE_PATH,
+                "Создание дневника ИИ",
+                new_content
+            )
+
+        print("Дневник обновлен в GitHub")
+
+    except Exception as e:
+        print("Ошибка GitHub:", e)
