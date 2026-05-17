662f9e569d21a52498a8a21c1be094a4f2e37c42
gdrga1063-ctrl <gdrga1063@gmail.com>
2026-05-18 00:53:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,13 +49,21 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # --- ЗАПУСК ---
 def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+    async def error_handler(update, context):
+        print(f"Ошибка Telegram: {context.error}")
 
+    app = ApplicationBuilder().token(TOKEN).build()
+        
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
+    app.add_error_handler(error_handler)
+
     print("Бот запущен...")
-    app.run_polling(drop_pending_updates=True)
+    app.run_polling(
+        drop_pending_updates=True,
+        close_loop=False
+    )
 
 if __name__ == "__main__":
     main()
